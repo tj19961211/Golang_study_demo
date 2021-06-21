34e0bb668c5c3eae3ec45eb7c387fda4a9ab1932
Clown <tj1211keynote@outlook.com>
2021-06-21 16:24:32 +0800

    完成结构体，方法和接口文章的跟敲，了解了表格驱动测试与辅助函数的使用

--- learn_Go_with_tests/struct_func_and_interface/Perimeter.go
+++ learn_Go_with_tests/struct_func_and_interface/Perimeter.go
@@ -1,16 +1,71 @@
 package struct_func_and_interface
 
+import "math"
+
+/*
+什么是方法？
+
+到目前为止我们只编写过函数但是我们已经使用过方法。当我们调用 t.Errorf 时我们调用了 t(testing.T) 这个实例的方法 ErrorF。
+
+方法和函数很相似但是方法是通过一个特定类型的实例调用的。函数可以随时被调用，比如 Area(rectangle)。不像方法需要在某个事物上调用。
+
+*/
+
+/*
+稍等，什么？
+
+这种定义 interface 的方式与大部分其他编程语言不同。通常接口定义需要这样的代码 My type Foo implements interface Bar。
+
+但是在我们的例子里，
+
+ - Rectangle 有一个返回值类型为 float64 的方法 Area，所以它满足接口 Shape
+
+ - Circle 有一个返回值类型为 float64 的方法 Area，所以它满足接口 Shape
+
+ - string 没有这种方法，所以它不满足这个接口
+
+  等等
+
+在 Go 语言中 interface resolution 是隐式的。如果传入的类型匹配接口需要的，则编译正确。
+
+解耦
+
+请注意我们的辅助函数是怎样实现不需要关心参数是矩形，圆形还是三角形的。通过声明一个接口，辅助函数能从具体类型解耦而只关心方法本身需要做的工作。
+
+这种方法使用接口来声明我们仅仅需要的。这种方法在软件设计中非常重要，我们以后在后续部分中还是涉及到更多细节。
+*/
 
 func (r *Rectangle) Perimeter() float64 {
-	return 2*(r.Width + r.Height)
+	return 2 * (r.Width + r.Height)
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
+func (r *Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c *Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (c *Triangle) Area() float64 {
+	return (c.Base * c.Height) * 0.5
 }
 
 type Rectangle struct {
-	Width float64
+	Width  float64
 	Height float64
 }
 
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+//这样我们就像创建 Rectangle 和 Circle 一样创建了一个新类型，不过这次是 interface 而不是 struct
+type Shape interface {
+	Area() float64
+}
--- learn_Go_with_tests/struct_func_and_interface/Perimeter_test.go
+++ learn_Go_with_tests/struct_func_and_interface/Perimeter_test.go
@@ -13,10 +13,114 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	// 像其他练习一样我们创建了一个辅助函数，但不同的是我们传入了一个 Shape 类型。
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := &Rectangle{12.0, 6.0}
+		checkArea(t, rectangle, 72.0)
+		//got := rectangle.Area()
+		// want := 100.0
+
+		// if got != want {
+		// 	t.Errorf("got %.2f want %.2f", got, want)
+		// }
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := &Circle{Radius: 10}
+		checkArea(t, circle, 314.1592653589793)
+		// got := circle.Area()
+		// want := 314.16
+
+		// if got != want {
+		// 	t.Errorf("got %.2f want %.2f", got, want)
+		// }
+	})
+}
+
+/*
+现在我们对结构体有一定的理解了，我们可以引入「表格驱动测试」。
+
+表格驱动测试(https://github.com/golang/go/wiki/TableDrivenTests)在我们要创建一系列相同测试方式的测试用例时很有用。
+*/
+
+/*
+列表驱动测试可以成为你工具箱中的得力武器。
+
+但是确保你在测试中真的需要使用它。
+
+如果你要测试一个接口的不同实现，或者传入函数的数据有很多不同的测试需求，这个武器将非常给力。
+*/
+
+/*
+
+ */
+
+func TestArea2(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{&Rectangle{12, 6}, 72.0},
+		{&Circle{10}, 314.1592653589793},
+		{&Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
 	}
 }
+
+/*
+下面是满足要求的最终测试代码：
+*/
+func TestArea3(t *testing.T) {
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Rectangle", shape: Rectangle{Radius: 12, Height: 6}, hasArea: 72.0},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
+}
+
+/*
+总结
+
+这是进一步的 TDD 实践。我们在对一个基本数学问题的解决方案的迭代中，通过测试学习了语言的新特性。
+
+ - 声明结构体以创建我们自己的类型，让我们把数据集合在一起并达到简化代码的目地
+
+ - 声明接口，这样我们可以定义适合不同参数类型的函数（参数多态）
+
+ - 在自己的数据类型中添加方法以实现接口
+
+ - 列表驱动测试让断言更清晰，这样可以使测试文件更易于扩展和维护
+
+这是重要的一课。因为我们开始定义自己的类型。在像 Go 这样的静态语言中，能定义自己的类型是开发易维护，低耦合，好测试的软件的基础。
+
+接口是把负责从系统的其他部分隐藏起来的伟大工具。在我们的测试中，辅助函数的代码不需要知道具体的几何形状，只需要知道获取它的面积即可。
+*/
